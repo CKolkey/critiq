14f1486edd1baf7bd3432a9227362c9f0fb88141
Cameron Kolkey <Alleyria@gmail.com>
2019-06-13 09:00:53 +0200

    Minor changes to devise's oauth logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,25 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid, team_id: auth.info.team_id).first_or_create do |user|
       user.provider = auth.provider
-      user.first_name = auth.info[:name].split.first
-      user.last_name = auth.info[:name].split.last
+      if auth.info[:name].nil?
+        user.first_name = auth.info[:user]
+        user.last_name = ""
+      else
+        user.first_name = auth.info[:name].split.first
+        user.last_name = auth.info[:name].split.last
+      end
       user.uid = auth.uid
       user.team_id = auth.info.team_id
-      user.nickname = auth.info.nickname
+      user.nickname = auth.info.nickname unless auth.info.nickname.nil?
       user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.slack_data"] && session["devise.slack_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 end
