4339604441b4efaef5ea056fb13f8af912aaa80c
Cameron Kolkey <Alleyria@gmail.com>
2019-06-13 12:12:54 +0200

    Async job set up

--- app/jobs/handle_slack_webhook_job.rb
+++ app/jobs/handle_slack_webhook_job.rb
@@ -2,8 +2,14 @@ class HandleSlackWebhookJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    event = args[0]['event']
-    puts event
-    # Do something later
+    event_hash = args[0][:event]
+    team_id = event_hash["team_id"]
+    text = event_hash["event"]["text"]
+    user_id = event_hash["event"]["user"]
+    channel = event_hash["event"]["channel"]
+    # event_ts = args[0][:event]["event"]["event_ts"]
+
+
+    puts "Jobe well done!"
   end
 end
