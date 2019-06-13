a88df901fd8ac023fe9d567bc2d3ea567e1839c4
Cameron Kolkey <Alleyria@gmail.com>
2019-06-13 09:16:28 +0200

    Added PSD files and a DB migration

---
+++ db/migrate/20190609125454_add_slackbot_server_tables.rb
@@ -0,0 +1,15 @@
+class AddSlackbotServerTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teams do |t|
+      t.string :team_id
+      t.string :name
+      t.boolean :active, default: true
+      t.string :domain
+      t.string :token
+      t.string :bot_user_id
+      t.string :activated_user_id
+      t.string :activated_user_access_token
+      t.timestamps
+    end
+  end
+end
